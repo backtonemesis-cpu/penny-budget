ddc0cc53bf5e3b118be345dcef068c37cd2c57d3
backtonemesis-cpu <backtonemesis@gmail.com>
2026-07-20 09:25:52 +0100

    Load the professional mobile navigation styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './navigation-height-fix.css';
+import './mobile-navigation.css';
 
 function resetHorizontalPosition() {
   document.documentElement.scrollLeft = 0;
