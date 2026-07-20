4af1f0f6228cfee6ccf319bc51a8cf056951f012
backtonemesis-cpu <backtonemesis@gmail.com>
2026-07-20 07:44:16 +0100

    Load the taller bottom navigation styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './navigation-height-fix.css';
 
 function resetHorizontalPosition() {
   document.documentElement.scrollLeft = 0;
