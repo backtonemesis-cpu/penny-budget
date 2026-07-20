40dc42868ddf59bc64d1fcea5d396e7c0526d478
backtonemesis-cpu <backtonemesis@gmail.com>
2026-07-20 20:18:55 +0100

    Load overview cleanup styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './mobile-navigation.css';
+import './overview-cleanup.css';
 
 function resetHorizontalPosition() {
   document.documentElement.scrollLeft = 0;
