0b6e72ddfebe0464da024846f4f1b5f441fa6249
backtonemesis-cpu <backtonemesis@gmail.com>
2026-08-31 10:33:53 +0100

    Enable Transfer Plan tab transform

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { recordDateLayoutPlugin } from './build/record-date-layout.js'
+import { transferPlanTabPlugin } from './build/transfer-plan-tab.js'
 
 // https://vitejs.dev
 export default defineConfig({
-  plugins: [recordDateLayoutPlugin(), react()],
+  plugins: [recordDateLayoutPlugin(), transferPlanTabPlugin(), react()],
   base: '/penny-budget/',
   build: {
     outDir: 'dist',
