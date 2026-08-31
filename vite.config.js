5e656b3638cb021050c69709203333c4f106e6ac
backtonemesis-cpu <backtonemesis@gmail.com>
2026-08-31 13:30:26 +0100

    Register desktop month picker plugin

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { recordDateLayoutPlugin } from './build/record-date-layout.js'
 import { primaryNavV64Plugin } from './build/primary-nav-v64.js'
 import { transferPlanTabPlugin } from './build/transfer-plan-tab.js'
 import { monthSelectorV74Plugin } from './build/month-selector-v74.js'
+import { desktopMonthPickerV75Plugin } from './build/desktop-month-picker-v75.js'
 
 // https://vitejs.dev
 export default defineConfig({
-  plugins: [recordDateLayoutPlugin(), primaryNavV64Plugin(), transferPlanTabPlugin(), monthSelectorV74Plugin(), react()],
+  plugins: [recordDateLayoutPlugin(), primaryNavV64Plugin(), transferPlanTabPlugin(), monthSelectorV74Plugin(), desktopMonthPickerV75Plugin(), react()],
   base: '/penny-budget/',
   build: {
     outDir: 'dist',
