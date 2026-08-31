fd8e700147ac6124f04eaa849e56c1fab4bb3a02
backtonemesis-cpu <backtonemesis@gmail.com>
2026-08-31 20:06:26 +0100

    Load v93 Add hub styles

--- src/main.jsx
+++ src/main.jsx
@@ -32,6 +32,7 @@ import './mobile-month-label-v74.css';
 import './desktop-month-picker-v75.css';
 import './assignment-row-compact-v89.css';
 import './add-hub-v92.css';
+import './add-hub-v93.css';
 
 let releaseCheckPromise = null;
 let lastReleaseCheckAt = 0;
