713c52b7e6cc9b0e57102819d25ef3c36479745f
backtonemesis-cpu <backtonemesis@gmail.com>
2026-08-31 12:59:50 +0100

    Enable v74 short month display transform

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import { recordDateLayoutPlugin } from './build/record-date-layout.js'
 import { primaryNavV64Plugin } from './build/primary-nav-v64.js'
 import { transferPlanTabPlugin } from './build/transfer-plan-tab.js'
+import { monthSelectorV74Plugin } from './build/month-selector-v74.js'
 
 // https://vitejs.dev
 export default defineConfig({
-  plugins: [recordDateLayoutPlugin(), primaryNavV64Plugin(), transferPlanTabPlugin(), react()],
+  plugins: [recordDateLayoutPlugin(), primaryNavV64Plugin(), transferPlanTabPlugin(), monthSelectorV74Plugin(), react()],
   base: '/penny-budget/',
   build: {
     outDir: 'dist',
