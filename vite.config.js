63be613bd7d22310e32efe595361e96cf7edb973
backtonemesis-cpu <backtonemesis@gmail.com>
2026-08-31 10:39:25 +0100

    Normalize primary nav before Transfer Plan transform

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { recordDateLayoutPlugin } from './build/record-date-layout.js'
+import { primaryNavV64Plugin } from './build/primary-nav-v64.js'
 import { transferPlanTabPlugin } from './build/transfer-plan-tab.js'
 
 // https://vitejs.dev
 export default defineConfig({
-  plugins: [recordDateLayoutPlugin(), transferPlanTabPlugin(), react()],
+  plugins: [recordDateLayoutPlugin(), primaryNavV64Plugin(), transferPlanTabPlugin(), react()],
   base: '/penny-budget/',
   build: {
     outDir: 'dist',
